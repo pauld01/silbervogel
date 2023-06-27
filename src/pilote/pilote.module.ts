8ddac56158d4592f03d5958c20dc8ce28e4ea30c
pauld01 <paul.duplessi@hotmail.fr>
2023-06-27 23:50:27 +0200

    add avions routesto pilote controller

--- src/pilote/pilote.module.ts
+++ src/pilote/pilote.module.ts
@@ -5,6 +5,8 @@ import { PiloteController } from './pilote.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Pilote } from './entities/pilote.entity';
 import { PiloteSchema } from './schemas/pilote.schemas';
+import { Avion, AvionSchema } from '../avion/schemas/avion.schemas'; // Mettez le chemin correct vers le fichier avion.schemas.ts
+import { AvionModule } from '../avion/avion.module'; // Mettez le chemin correct vers le fichier avion.module.ts
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -12,7 +14,9 @@ dotenv.config();
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Pilote.name, schema: PiloteSchema }]),
+    MongooseModule.forFeature([{ name: Avion.name, schema: AvionSchema }]),
     MongooseModule.forRoot(process.env.DB_URL_PILOTE),
+    AvionModule,
   ],
   controllers: [PiloteController],
   providers: [PiloteService],
