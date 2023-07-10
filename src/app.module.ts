7ec9eca1db0db486c63ac9eec02072daffcafe68
kevin-fer <kevin.ferreira@efrei.net>
2023-07-10 10:44:53 +0200

    correctif

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,9 @@ import { PiloteModule } from './pilote/pilote.module';
 import { VolModule } from './vol/vol.module';
 import { AeroportModule } from './aeroport/aeroport.module';
 import { AvionModule } from './avion/avion.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [PiloteModule,AeroportModule,AvionModule,VolModule],
+  imports: [PiloteModule,AeroportModule,AvionModule,VolModule, ConfigModule.forRoot() ],
   controllers: [AppController],
   providers: [AppService],
 })
