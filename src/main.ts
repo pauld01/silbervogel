55aaa651a136fa71713f22e00d4758e293c2dea4
pauld01 <paul.duplessi@hotmail.fr>
2023-07-11 10:16:46 +0200

    add rabit

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config as dotenvConfig } from 'dotenv';
 import amqp from 'amqp-connection-manager';
+import { Transport } from '@nestjs/microservices';
 async function bootstrap() {
   dotenvConfig();
 
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
-}
-bootstrap.then();
-
-var connection = amqp.connect(['amqp://localhost:3000', 'amqp://localhost:3002', 'amqp://localhost:3003']);
-var channelWrapper = connection.createChannel({
-  json: true,
-  setup: function (channel) {
-    return channel.assertQueue('avions_queue', { durable: true });
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'flight_queue',
+      queueOptions: {
+        durable: false,
+      },
     },
-});
+  });
+
+  await app.listen(); // Launch the microservice
+
+  // Define your channelWrapper here
+}
+bootstrap();
+const channelWrapper = amqp.connect(['amqp://localhost:5672']);
 
 export default channelWrapper;
